5eab16361b3d855c3770b7dd821be46f94fbf161
Isac Hansson <isachansson1218@gmail.com>
2025-04-16 11:59:32 +0200

    Changed styling for map, add zoom and drag feature

--- webapp/js/votingMap.js
+++ webapp/js/votingMap.js
@@ -4,19 +4,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const container = d3.select("#voting-map-container");
   if (container) {
     const svg = d3.select("#geo-map");
-  const containerWidth = container.node().getBoundingClientRect().width;
-  const containerHeight = containerWidth * 0.75;
 
-
-  svg.attr("width", containerWidth).attr("height", containerHeight);
-  const projection = d3.geoMercator()
-    .scale(containerWidth / 6) 
-    .translate([containerWidth / 2, containerHeight / 2]);
+    const containerWidth = container.node().getBoundingClientRect().width;
+    const containerHeight = containerWidth * 0.75;
+  
+    svg.attr("width", containerWidth).attr("height", containerHeight);
+  
+    const projection = d3.geoMercator()
+      .scale(containerWidth / 6)
+      .translate([containerWidth / 6, containerHeight / 1.8]);
   
     const path = d3.geoPath().projection(projection);
+
+    const mapGroup = svg.append("g");
     d3.json("data/map.geo.json")
     .then(geoData => {
-      svg.selectAll("path")
+      mapGroup.selectAll("path")
         .data(geoData.features)
         .enter()
         .append("path")
@@ -24,25 +27,15 @@ window.addEventListener("DOMContentLoaded", () => {
         .attr("stroke", "#fff");
     })
     .catch(error => console.error("Error loading GeoJSON data:", error));
+    const zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", (event) => {
+      mapGroup.attr("transform", event.transform);
+    });
 
+  svg.call(zoom);
   } else {
     console.warn("Couldn't find #voting-map-container");
   }
-
-
 });
 
-window.addEventListener("resize", () => {
-  const container = d3.select("#voting-map-container");
-  const svg = d3.select("#geo-map");
-
-  const containerWidth = container.node().getBoundingClientRect().width;
-  const containerHeight = containerWidth * 0.75;
-
-  svg.attr("width", containerWidth).attr("height", containerHeight);
-  const projection = d3.geoMercator()
-    .scale(containerWidth / 6)
-    .translate([containerWidth / 2, containerHeight / 2]);
-  const path = d3.geoPath().projection(projection);
-  svg.selectAll("path").attr("d", path);
-});
