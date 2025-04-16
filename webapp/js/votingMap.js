b6037ca464900bb71da31cbe418d40a8a8621a49
Isac Hansson <isachansson1218@gmail.com>
2025-04-16 10:58:13 +0200

    Init map voting

--- webapp/js/votingMap.js
+++ webapp/js/votingMap.js
@@ -1,12 +1,48 @@
-console.log("Hello from votingMap.js");
+
 
 window.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("voting-map-container");
+  const container = d3.select("#voting-map-container");
   if (container) {
-    const message = document.createElement("p");
-    message.textContent = "From votingMap.js!";
-    container.appendChild(message);
+    const svg = d3.select("#geo-map");
+  const containerWidth = container.node().getBoundingClientRect().width;
+  const containerHeight = containerWidth * 0.75;
+
+
+  svg.attr("width", containerWidth).attr("height", containerHeight);
+  const projection = d3.geoMercator()
+    .scale(containerWidth / 6) 
+    .translate([containerWidth / 2, containerHeight / 2]);
+  
+    const path = d3.geoPath().projection(projection);
+    d3.json("data/map.geo.json")
+    .then(geoData => {
+      svg.selectAll("path")
+        .data(geoData.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("stroke", "#fff");
+    })
+    .catch(error => console.error("Error loading GeoJSON data:", error));
+
   } else {
     console.warn("Couldn't find #voting-map-container");
   }
+
+
+});
+
+window.addEventListener("resize", () => {
+  const container = d3.select("#voting-map-container");
+  const svg = d3.select("#geo-map");
+
+  const containerWidth = container.node().getBoundingClientRect().width;
+  const containerHeight = containerWidth * 0.75;
+
+  svg.attr("width", containerWidth).attr("height", containerHeight);
+  const projection = d3.geoMercator()
+    .scale(containerWidth / 6)
+    .translate([containerWidth / 2, containerHeight / 2]);
+  const path = d3.geoPath().projection(projection);
+  svg.selectAll("path").attr("d", path);
 });
